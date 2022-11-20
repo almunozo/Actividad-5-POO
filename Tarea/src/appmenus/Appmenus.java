f2b4d5467ef6576a31ea84b3280108ae808eef7b
almunozo <almunozo@unal.edu.co>
2022-11-19 22:45:42 -0500

    Trabajo 5

---
+++ Tarea/src/appmenus/Appmenus.java
@@ -0,0 +1,17 @@
+package appmenus;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author santi
+ */
+public class Appmenus {
+    public static void main(String[] args){
+    frmPrincipal principal = new frmPrincipal();
+    principal.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    principal.setVisible(true);
+    
+    
+    }
+    
+}
---
+++ Tarea/src/appmenus/Voltaje.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package appmenus;
+
+/**
+ *
+ * @author santi
+ */
+public class Voltaje {
+    public static double calcular_voltaje(double corriente,double resistencia){
+        double voltaje;
+        voltaje=corriente*resistencia;
+        return voltaje;
+    }
+}
